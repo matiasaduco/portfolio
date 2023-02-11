525d67ef538cac3848bba2f5be62d9ed3ce15c36
Matías Aduco <matias.aduco@gmail.com>
2023-02-11 19:11:28 -0300

    Correcciones al CSS de ProjectCard. Agregado vídeo de D&D para la vista previa. Aún no está funcional.

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,12 +1,17 @@
+import video from '../D.mp4';
+
 const ProjectCard = ({ img, title, description }) => {
   return (
-    <span>
+    <a href='#projectdd' className='card'>
       <section>
+        {/* <video autoPlay loop muted width='320' height='240'>
+          <source src={video} type='video/mp4' />
+        </video> */}
         <img src={img} alt='project1' />
       </section>
       <b>{title}</b>
       <i>{description}</i>
-    </span>
+    </a>
   );
 };
 

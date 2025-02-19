18cce488fcebc5286cba8c671b2839347fc5ccf5
Matias Aduco <matias.aduco@gmail.com>
2025-02-19 14:14:45 -0300

    Instalado PrimeReact
    Reemplazo de Bootstrap por Tailwind
    
    Instalado tsParticles

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,25 @@
-import './App.css';
+import './App.css'
+import 'primereact/resources/themes/lara-light-cyan/theme.css'
+import 'primeicons/primeicons.css'
 import NavBar from './app/NavBar/NavBar';
-import AboutMe from './app/AboutMe/AboutMe';
+import AboutMe from './app/AboutMe/AboutMe'
 import Skills from './app/Skills/Skills';
 import Projects from './app/Projects/Projects';
 import Contact from './app/Contact/Contact';
 import Datos from './app/Datos/Datos';
+import { PrimeReactProvider } from 'primereact/api'
 
 function App() {
   return (
-    <>
+    <PrimeReactProvider>
       <AboutMe />
-      <NavBar />
-      <Datos />
-      <Skills />
-      <Projects />
-      <Contact />
-    </>
-  );
+      {/* <NavBar /> */}
+      {/* <Datos /> */}
+      {/* <Skills /> */}
+      {/* <Projects /> */}
+      {/* <Contact /> */}
+    </PrimeReactProvider>
+  )
 }
 
-export default App;
+export default App
--- src/app/AboutMe/AboutMe.jsx
+++ src/app/AboutMe/AboutMe.jsx
@@ -3,45 +3,40 @@ import perfil from '@/assets/images/perfil.jpg'
 // import github from '@/assets/images/github.png'
 // import linkedin from '@/assets/images/linkedin.png'
 import cv from '@/assets/downloads/CV_2023022817213428.pdf'
+import { Button } from 'primereact/button'
+import ReactParticles from '../../components/ReactParticles'
 
 const AboutMe = () => {
   return (
-    <div id='inicio' className='container-fluid w-50 vh-100 d-flex'>
-      <div className='row gap-5 justify-content-center align-items-center'>
-        <img className='profile col-3' src={perfil} alt='Foto de Perfil' />
-        <div className='col-8 row gap-3 justify-content-center'>
-          <div className='biography col-12'>
-            Hola, soy <strong>Matías Aduco</strong>
-            <br />
-            Desarrollador Full Stack
-            <br />
-            Me encuentro cursando la carrera de Licenciatura en Informática,
-            también soy Docente Auxiliar Universitario en la UNQui. Actualmente
-            me encuentro en búsqueda de un trabajo como desarrollador. Pueden
-            descargar mi Curriculum justo aquí debajo. Mis redes se encuentran
-            al pie de la página!
-          </div>
-          <div className='row gap-2 col-12'>
-            <a
-              href={cv}
-              download='Matias Gonzalez Aduco.pdf'
-              className='btn btn-outline-primary rounded-pill col-2'
-            >
-              Descargar CV
-            </a>
-            <button
-              className='btn btn-outline-primary rounded-pill col-2'
-              type='submit'
-            >
-              GitHub
-            </button>
-            <button
-              className='btn btn-outline-primary rounded-pill col-2'
-              type='submit'
-            >
-              Linkedin
-            </button>
-          </div>
+    <div
+      id='inicio'
+      className='flex md:flex-col lg:flex-row h-screen justify-center items-center gap-12'
+      style={{ width: '75%', margin: 'auto' }}
+    >
+      <ReactParticles />
+      <img className='profile w-100' src={perfil} alt='Foto de Perfil' />
+      <div className='flex flex-col gap-4'>
+        <div className='biography'>
+          Hola, soy <strong>Matías Aduco</strong>
+          <br />
+          Desarrollador Full Stack
+          <br />
+          Me encuentro cursando la carrera de Licenciatura en Informática,
+          también soy Docente Auxiliar Universitario en la UNQui. Actualmente me
+          encuentro en búsqueda de un trabajo como desarrollador. Pueden
+          descargar mi Curriculum justo aquí debajo. Mis redes se encuentran al
+          pie de la página!
+        </div>
+        <div className='flex gap-4 md:justify-center lg:justify-start'>
+          <a
+            href={cv}
+            download='Matias Gonzalez Aduco.pdf'
+            className='p-button font-bold p-button-rounded p-button-outlined'
+          >
+            Descargar CV
+          </a>
+          <Button label='GitHub' icon='pi pi-github' rounded outlined />
+          <Button label='Linkedin' icon='pi pi-linkedin' rounded outlined />
         </div>
       </div>
     </div>

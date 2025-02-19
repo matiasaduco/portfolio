3954a4519b6380e18d573b36a6e565a05b332b8d
Matias Aduco <matias.aduco@gmail.com>
2025-02-19 00:31:53 -0300

    Cambios en AboutMe

--- src/app/AboutMe/AboutMe.jsx
+++ src/app/AboutMe/AboutMe.jsx
@@ -6,23 +6,20 @@ import cv from '@/assets/downloads/CV_2023022817213428.pdf'
 
 const AboutMe = () => {
   return (
-    <div
-      id='inicio'
-      className='container-fluid vh-100 d-flex'
-    >
+    <div id='inicio' className='container-fluid w-50 vh-100 d-flex'>
       <div className='row gap-5 justify-content-center align-items-center'>
         <img className='profile col-3' src={perfil} alt='Foto de Perfil' />
         <div className='col-8 row gap-3 justify-content-center'>
           <div className='biography col-12'>
             Hola, soy <strong>Matías Aduco</strong>
-            <div>
-              Soy programador Junior Full Stack. Aún me encuentro cursando la
-              carrera de Licenciatura en Informática, también soy Docente
-              Auxiliar Universitario en la UNQui. Actualmente me encuentro en
-              búsqueda de un trabajo como desarrollador. Pueden descargar mi
-              Curriculum justo aquí debajo. Mis redes se encuentran al pie de la
-              página!
-            </div>
+            <br />
+            Desarrollador Full Stack
+            <br />
+            Me encuentro cursando la carrera de Licenciatura en Informática,
+            también soy Docente Auxiliar Universitario en la UNQui. Actualmente
+            me encuentro en búsqueda de un trabajo como desarrollador. Pueden
+            descargar mi Curriculum justo aquí debajo. Mis redes se encuentran
+            al pie de la página!
           </div>
           <div className='row gap-2 col-12'>
             <a

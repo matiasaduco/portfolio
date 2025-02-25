4534147c61f7b6df3c5273f9e0b2b6721c73fa24
Matias Aduco <matias.aduco@gmail.com>
2025-02-25 20:51:06 -0300

    Cambios en Assets

--- src/assets/icons/languages/frontend/Frontend.js
+++ src/assets/icons/languages/frontend/Frontend.js
@@ -4,7 +4,8 @@ import css from './css3.png'
 import html from './html5.png'
 import javascript from './javascript.png'
 import typescript from './typescript.svg'
+import tailwind from './tailwind.png'
 
-const frontend = [vue, react, html, css, javascript, typescript]
+const frontend = [vue, react, html, css, javascript, typescript, tailwind]
 
 export default frontend
